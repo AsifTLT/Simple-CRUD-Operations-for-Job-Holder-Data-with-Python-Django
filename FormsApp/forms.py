7042513e50b5151cbcd429050d32de0d2708af4b
Mohammad Asif <akonasif321@gmail.com>
2024-07-05 01:11:44 +0600

    Initial commit

---
+++ FormsApp/forms.py
@@ -0,0 +1,7 @@
+from django import forms # type: ignore
+from django.contrib.auth.models import User # type: ignore
+
+
+# class userForm(forms.Form):
+#     name = forms.CharField()
+#     password = forms.IntegerField(widget=forms.PasswordInput)
